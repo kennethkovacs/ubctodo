31064279ee72ec43dad9acdb07355cabd608cfda
kennethkovacs <17244596+kennethkovacs@users.noreply.github.com>
2022-01-15 18:17:11 -0800

    replacing basic templates with react

--- script.js
+++
@@ -1,9 +0,0 @@
-const submitbutton = document.querySelector(".submitbutton")
-const divParent = document.querySelector(".container")
-
-submitbutton.addEventListener('mouseover', function(){
-	console.log('mouse over')
-    const div  = document.createElement('div');
-    divParent.appendChild(div)
-})
-
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <h1>hello world!!</h1>
+    );
+  }  
+}
+
+export default Hello;
